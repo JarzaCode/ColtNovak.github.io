b540df4de287e7ac3d7bb26f2ece4f6123a1516f
Colt Novak <colton.novak@students.coderva.org>
2024-12-17 14:34:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const ScrollReveal = require('scrollreveal')
 
 document.addEventListener('DOMContentLoaded', () => {
     const themeToggleBtn = document.getElementById('theme-toggle');
@@ -12,25 +11,4 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem('theme', newTheme);
     });
 
-    const sr = ScrollReveal({
-        origin: 'bottom',    
-        distance: '50px',    
-        duration: 1000,       
-        delay: 200,           
-        opacity: 0,           
-        scale: 0.8,           
-        easing: 'ease-out',   
-        reset: false,         
-    });
 
-    sr.reveal('.section', { interval: 300 }); 
-    sr.reveal('#intro', { interval: 300 });
-    sr.reveal('#skills', { interval: 300 });
-    sr.reveal('#projects', { interval: 300 });
-    sr.reveal('#experience', { interval: 300 });
-    sr.reveal('#contact', { interval: 300 });
-
-    sr.reveal('.skill-card', { interval: 100 });
-    sr.reveal('.project-card', { interval: 100 });
-    sr.reveal('.experience-card', { interval: 100 });
-});
