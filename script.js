d8058a3ae0586490a5387c7cd0506dbdd1f901f0
Colt Novak <colton.novak@students.coderva.org>
2024-12-17 14:46:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggleBtn = document.getElementById('theme-toggle');
-    const currentTheme = localStorage.getItem('theme') || 'dark';
-
-    document.documentElement.setAttribute('data-theme', currentTheme);
-
-    themeToggleBtn.addEventListener('click', () => {
-        const newTheme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
-        document.documentElement.setAttribute('data-theme', newTheme);
-        localStorage.setItem('theme', newTheme);
-    });
-
-
