3d8dc855b72b159da42b1bb1cd3805c7ed0fe8ab
Colt Novak <colton.novak@students.coderva.org>
2024-12-17 16:14:53 +0000

    Your commit message

--- src/toggleTheme.js
+++ src/toggleTheme.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const themeToggleBtn = document.getElementById('theme-toggle');
-  const htmlElement = document.documentElement;
-
-  themeToggleBtn.addEventListener('click', () => {
-    if (htmlElement.getAttribute('data-theme') === 'dark') {
-      htmlElement.setAttribute('data-theme', 'light');
-    } else {
-      htmlElement.setAttribute('data-theme', 'dark');
-    }
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    const htmlElement = document.documentElement;
+  
+    themeToggleBtn.addEventListener('click', () => {
+      if (htmlElement.getAttribute('data-theme') === 'dark') {
+        htmlElement.setAttribute('data-theme', 'light');
+      } else {
+        htmlElement.setAttribute('data-theme', 'dark');
+      }
+    });
   });
-});
